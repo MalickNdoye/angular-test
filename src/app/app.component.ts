6753735c342e864991f0bdb67f668f99c4c3bd10
MalickNdoye <malickndoye90@hotmail.fr>
2021-01-31 02:08:23 +0100

    First save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-test';
+  title = 'angular-test : initiation';
+  isAuth = false ;
+
+  lastUpdate = new Date() ;
+
+  appareils = [
+    {
+      name: 'Siri',
+      status: 'allumé',
+    },
+    {
+      name: 'Cortana',
+      status: 'allumé',
+    },
+    {
+      name: 'Alexa',
+      status: 'éteint',
+    },
+  ] ;
+
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllume(): void{
+    console.log('On allume tout.');
+  }
 }
